d92fc1d596a0d9b14eba0f7b546f270a407dea55
Mahyad <saedpanahmahyad@gmail.com>
2025-04-12 14:12:57 +0330

    initial commit

--- code/cifar100/flatness_minima.py
+++ code/cifar100/flatness_minima.py
@@ -8,69 +8,6 @@ class SAM:
         self.rho = rho
         self.eta = eta
         self.state = defaultdict(dict)
-        self.epsilon = 1e-3
-
-    @torch.no_grad()
-    def perturb_step(self):
-        # محاسبه نرم گرادیان
-        grads = []
-        for n, p in self.model.named_parameters():
-            if p.grad is None:
-                continue
-            grads.append(torch.norm(p.grad, p=2))
-        grad_norm = torch.norm(torch.stack(grads), p=2) + 1.e-16
-        self.grad_norm = grad_norm
-
-        # ذخیره پارامترهای و گرادیان‌های اصلی
-        original_params = {}
-        original_grads = {}
-        for n, p in self.model.named_parameters():
-            if p.grad is not None:
-                original_params[n] = p.data.clone()
-                original_grads[n] = p.grad.clone()  # ∇wL'(W,X,Y)
-
-        for n, p in self.model.named_parameters():
-            if p.grad is None:
-                self.state[p]["eps"] = torch.zeros_like(p)
-                continue
-
-            # محاسبه Ŵ-W
-            eps = torch.clone(p.grad).detach()
-            eps.mul_(self.rho / grad_norm)
-
-            # محاسبه گرادیان در W + ε'(Ŵ-W) برای داده‌های اصلی
-            p.data.add_(self.epsilon * eps)
-            grad_new_orig = torch.zeros_like(p.grad)
-            if p.grad is not None:
-                grad_new_orig.copy_(p.grad)  # ∇wL'(W + ε'(Ŵ-W),X,Y)
-
-            # محاسبه گرادیان در W + ε'(Ŵ-W) برای داده‌های mixup
-            grad_new_mix = torch.zeros_like(p.grad)
-            if p.grad is not None:
-                grad_new_mix.copy_(p.grad)  # ∇wL'(W + ε'(Ŵ-W),X̃,Ỹ)
-
-            # بازگشت به پارامترهای اصلی
-            p.data.copy_(original_params[n])
-
-            # محاسبه Hw(Ŵ-W) برای داده‌های اصلی (معادله 9)
-            hessian_term_orig = (grad_new_orig - original_grads[n]) / self.epsilon
-
-            # محاسبه Hw̃(Ŵ-W) برای داده‌های mixup (معادله 10)
-            hessian_term_mix = (grad_new_mix - original_grads[n]) / self.epsilon
-
-            # محاسبه گرادیان اصلاح شده (معادله 11)
-            modified_grad = original_grads[n] + hessian_term_orig
-
-            # اعمال معادلات 7 و 8
-            eps = original_grads[n].clone()
-            eps.mul_(self.rho / grad_norm)
-            eps.add_(0.5 * hessian_term_orig * eps)  # برای داده‌های اصلی
-            eps.add_(0.5 * hessian_term_mix * eps)   # برای داده‌های mixup
-
-            self.state[p]["eps"] = eps
-            p.add_(eps)
-
-        self.optimizer.zero_grad()
 
     @torch.no_grad()
     def perturb_step_nograd(self):
@@ -91,12 +28,34 @@ class SAM:
         self.optimizer.zero_grad()
 
     @torch.no_grad()
+    def perturb_step(self):
+        grads = []
+        for n, p in self.model.named_parameters():
+            if p.grad is None:
+                continue
+            grads.append(torch.norm(p.grad, p=2))
+        grad_norm = torch.norm(torch.stack(grads), p=2) + 1.e-16
+        self.grad_norm = grad_norm
+        for n, p in self.model.named_parameters():
+            if p.grad is None:
+                self.state[p]["eps"] = torch.zeros_like(p)
+                continue
+            eps = self.state[p].get("eps")
+            if eps is None:
+                eps = torch.clone(p).detach()
+            eps[...] = p.grad[...]
+            eps.mul_(self.rho / grad_norm)
+            self.state[p]["eps"] = eps
+            p.add_(eps)
+        self.optimizer.zero_grad()
+
+    @torch.no_grad()
     def unperturb_step(self):
         for n, p in self.model.named_parameters():
             if p.grad is None:
                 continue
-            if "eps" in self.state[p]:
-                p.sub_(self.state[p]["eps"])
+            # print(n)
+            p.sub_(self.state[p]["eps"])
 
     @torch.no_grad()
     def step(self):
